314587f1421a6c3dc32c84a9dae5abaa82f1efc2
Aaron Stephenson <aaron@bronron.com>
2024-06-26 00:28:50 +1000

    Updated swift package.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,20 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "CountryCode",
-    targets: [],
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+    ],
     dependencies: [
-        .Package(url: "git@github.com:aaron-mmw/CountryCode.git")
+        // Dependencies declare other packages that this package depends on.
+//        .package(url: "git@github.com:aaron-mmw/CountryCode.git", .branch("master"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
     ]
 )
