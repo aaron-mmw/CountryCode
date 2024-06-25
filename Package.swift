a9c83ff470f2773fb82a58adbaa5b20c04c70a21
Aaron Stephenson <aaron@bronron.com>
2024-06-26 09:31:58 +1000

    Updated package

--- Package.swift
+++ Package.swift
@@ -8,13 +8,26 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CountryCode",
+            targets: ["CountryCode"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-//        .package(url: "git@github.com:aaron-mmw/CountryCode.git", .branch("master"))
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "CountryCode",
+            dependencies: [],
+            path: "CountryPicker",
+            resources: [
+                .copy("Assets/CountryPicker.bundle")]
+        ),
+        .testTarget(
+            name: "CountryCodeTests",
+            dependencies: ["CountryCode"]),
     ]
 )
